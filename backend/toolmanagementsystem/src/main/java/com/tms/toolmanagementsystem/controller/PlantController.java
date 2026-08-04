940276d6c375a4ece606db6c10f6b43baa7ec726
Tejas Chougule <105717313+ItsTejashere@users.noreply.github.com>
2026-08-04 14:03:46 +0530

    Add cacheable annotation to PlantController

--- backend/toolmanagementsystem/src/main/java/com/tms/toolmanagementsystem/controller/PlantController.java
+++ backend/toolmanagementsystem/src/main/java/com/tms/toolmanagementsystem/controller/PlantController.java
@@ -5,6 +5,7 @@ import com.tms.toolmanagementsystem.repository.PlantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.cache.annotation.Cacheable;
 
 import java.util.List;
 
