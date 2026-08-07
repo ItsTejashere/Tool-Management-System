1fca26bf2f630a8624efa5c90f0234d2a1c06662
Tejas Chougule <105717313+ItsTejashere@users.noreply.github.com>
2026-08-07 15:45:23 +0530

    Refactor API_URL assignment in config.js
    Removed default API URL and console log statement.

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,4 @@
 // frontend/src/config.js
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
-
-console.log("VITE IS USING THIS BACKEND URL: ", API_URL);
+const API_URL = import.meta.env.VITE_API_URL;
 
 export default API_URL;
